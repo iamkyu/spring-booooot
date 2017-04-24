adf7072a2aa09e413b34ede0172b42b78b9b25da
iamkyu <kyujnam@gmail.com>
2017-04-25 08:04:45 +0900

    refactor: 당장 불필요한 상속 구조 제거

--- src/main/java/io/iamkyu/paging/Page.java
+++ src/main/java/io/iamkyu/paging/Page.java
@@ -1,18 +1,20 @@
 package io.iamkyu.paging;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * @author Kj Nam
  * @since 2017-04-21
  */
-public class Page<T> extends Chunk {
+public class Page<T> {
+    private final List<T> content = new ArrayList<>();
     private final long total;
     private final Pageable pageable;
 
     public Page(List<T> content, long total, Pageable pageable) {
-        super(content, pageable);
-
+        this.content.addAll(content);
         this.pageable = pageable;
         this.total = !content.isEmpty() && pageable != null && pageable.getOffset() + pageable.getPageSize() > total
                 ? pageable.getOffset() + content.size() : total;
@@ -20,5 +22,17 @@ public class Page<T> extends Chunk {
 
     public int getTotalPages() {
         return getSize() == 0 ? 1 : (int) Math.ceil((double) total / (double) getSize());
+    }
+
+    public int getNumber() {
+        return pageable == null ? 0 : pageable.getPageNumber();
+    }
+
+    public int getSize() {
+        return pageable == null ? 0 : pageable.getPageSize();
+    }
+
+    public List<T> getContent() {
+        return Collections.unmodifiableList(content);
     }
 }
